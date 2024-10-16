4e4b661b4849993a58d7a5d9b2d9c6c67f76c5a1
harshlaghave <145957194+harshlaghave@users.noreply.github.com>
2024-10-16 10:54:43 +0530

    dsps1.cpp

--- dsps1.cpp
+++ dsps1.cpp
@@ -1,10 +1,10 @@
-08.22 11:10 AM
+
 #include <iostream>
-#include<ctime>
+
 using namespace std;
 int main() {
     int a[10], n,temp,i,target,flag=0;
-    clock_t start,end;
+  
     cout << "Enter the size of an array: ";
     cin >> n;
     for (int i = 0; i < n; i++) {
@@ -17,7 +17,7 @@ int main() {
     }
     cout<<"\n Enter the Target :";
     cin>>target;
-    start=clock();
+ 
     for(i=0;i<n;i++){
         if(a[i]==target){
           flag=1;
@@ -35,8 +35,7 @@ int main() {
     if(flag==0){
         cout<<"Element Not Found";
     }
-    end = clock();
-    cout<<"\n Time taken is "<<double(end-start)/CLOCKS_PER_SEC<<" seconds"<<endl;
+    
     return 0;
 }
 
